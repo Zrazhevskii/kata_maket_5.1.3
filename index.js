d1df0eeef34ace32fdd2d7be4d7bcbe444ad42b8
Zrazhevskii <vbn-134@yandex.ru>
2024-05-15 19:23:35 +0300

    коррекция исправлений

--- index.js
+++ index.js
@@ -1,54 +1,88 @@
-const container = document.querySelector('.box__container');
-const containerChild = container.children;
 const btn = document.querySelector('.wrapper__sliders__btn');
 const wrapper = document.querySelector('.wrapper__sliders');
+const containerSlider = Array.from(
+    document.querySelectorAll('.container__slide')
+);
+
 let width = window.innerWidth;
 
 window.addEventListener('resize', () => {
     width = window.innerWidth;
     if (width >= 768) {
-        chekenChildren();
+        checkChildren();
     }
 });
 
 btn.addEventListener('click', function () {
     if (btn.className.includes('btn-show')) {
-        wrapper.classList.add('wrapper-show-more')
-        btn.classList.remove('btn-show');
-        btn.classList.add('btn-hide');
-        for (let i = 0; i < containerChild.length; i++) {
-            if (containerChild[i].className.includes('hidden')) {
-                containerChild[i].classList.remove('hidden');
-            }
-        }
+        changeHeightOpen();
     } else {
-        wrapper.classList.remove('wrapper-show-more')
-        btn.classList.add('btn-show');
-        btn.classList.remove('btn-hide');
-        for (let i = 0; i < containerChild.length; i++) {
-            if (i >= 6 && width < 1120) {
-                containerChild[i].classList.add('hidden');
-            }
-
-            if (width > 1120 && i >= 8) {
-                containerChild[i].classList.add('hidden');
-            }
-        }
+        changeHeightClose();
     }
 });
 
-function chekenChildren() {
+function checkChildren() {
     if (width > 768 && width < 1120) {
-        containerChild[6].classList.add('hidden');
-        containerChild[7].classList.add('hidden');
+        for (let i = 0; i < containerSlider.length; i++) {
+            if (i >= 6) {
+                containerSlider[i].classList.add('hidden');
+            }
+        }
     }
 
     if (width >= 1120) {
-        containerChild[6].classList.remove('hidden');
-        containerChild[7].classList.remove('hidden');
+        for (let i = 0; i < containerSlider.length; i++) {
+            if (i <= 7) {
+                containerSlider[i].classList.remove('hidden');
+            } else {
+                containerSlider[i].classList.add('hidden');
+            }
+        }
     }
-
-
 }
 
-chekenChildren();
+const changeHeightOpen = () => {
+    if (width >= 768 && width < 1120) {
+        wrapper.classList.add('wrapper-show-more');
+
+        for (let i = 0; i < containerSlider.length; i++) {
+            if (containerSlider[i].className.includes('hidden')) {
+                containerSlider[i].classList.remove('hidden');
+            }
+        }
+    } else {
+        wrapper.classList.add('wrapper-show-more-small');
+        for (let i = 0; i < containerSlider.length; i++) {
+            if (containerSlider[i].className.includes('hidden')) {
+                containerSlider[i].classList.remove('hidden');
+            }
+        }
+    }
+
+    btn.classList.remove('btn-show');
+    btn.classList.add('btn-hide');
+};
+
+const changeHeightClose = () => {
+    if (width >= 768 && width < 1120) {
+        wrapper.classList.remove('wrapper-show-more');
+
+        for (let i = 0; i < containerSlider.length; i++) {
+            if (i >= 6) {
+                containerSlider[i].classList.add('hidden');
+            }
+        }
+    } else if (width >= 1120) {
+        wrapper.classList.remove('wrapper-show-more-small');
+        for (let i = 0; i < containerSlider.length; i++) {
+            if (i >= 8) {
+                console.log(containerSlider[i]);
+                containerSlider[i].classList.add('hidden');
+            }
+        }
+    }
+    btn.classList.add('btn-show');
+    btn.classList.remove('btn-hide');
+};
+
+checkChildren();
--- slider.js
+++ slider.js
@@ -1,15 +1,48 @@
-new Swiper('.swiper-container', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    slidesPerView: 'auto',
-    spaceBetween: 16,
-    loop: true,
-    watchOverflow: true,
-    breakpoints: { 320: { enabled: true }, 768: { enabled: false } },
-});
+// new Swiper('.swiper-container', {
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     slidesPerView: 'auto',
+//     spaceBetween: 16,
+//     loop: true,
+//     watchOverflow: true,
+//     breakpoints: { 320: { enabled: true }, 768: { enabled: false } },
+// });
 
 const slider = document.querySelector('.swiper-container');
+const pagination = document.querySelector('.swiper-pagination');
 let { clientWidth } = document.body;
-// console.log(clientWidth);
+
+let yourSlider;
+
+const sliderInit = () => {
+    yourSlider = new Swiper(slider, {
+        pagination: {
+            el: pagination,
+            clickable: true,
+        },
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+        loop: true,
+    })
+}
+
+const resizeHandlerSlider = (number) => {
+    if (number >= 768) {
+        if (yourSlider) {
+            yourSlider.destroy();
+            return
+        }
+    }
+    sliderInit();
+}
+
+window.addEventListener('resize', () => {
+
+    width = window.innerWidth;
+    resizeHandlerSlider(width);
+
+});
+
+resizeHandlerSlider(clientWidth)
